9e733180a0b87deb6678812d6c2714f2685e29e5
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-11-08 15:17:49 +0100

    adding codes

--- client/src/api/modules.js
+++ client/src/api/modules.js
@@ -1,5 +1,17 @@
 const API_URL = 'http://localhost:4000'
 
+const head = {
+  'Content-Type': 'application/json'
+};
+
 export const getModules = () => {
   return fetch(`${API_URL}/module`).then(response => response.json());
+};
+
+export const createModules = (title) => {
+  return fetch(`${API_URL}/module`, {
+    method: 'POST',
+    headers: head, // this is where we set the headers
+    body: JSON.stringify({ title: title })
+  }).then(response => response.json());
 };
--- client/src/components/Modules.jsx
+++ client/src/components/Modules.jsx
@@ -1,32 +1,112 @@
 import React, { Component} from 'react';
-import { getModules } from '../api/modules';
+import { createModules, DeleteModules, getModules } from '../api/modules';
 
 class Modules extends Component {
-  state = {
-    modules: []
-  };
-
-  componentDidMount() {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      modules: [],
+      id: '',
+      filter: 'all',
+      searchString : '',
+    };
+  }
+ 
+   componentDidMount() {
     getModules().then((modules) => {
       this.setState({ modules: modules });
     });
   }
+ 
+  addNewModule = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+  addModule = e => {
+    e.preventDefault();
+    createModules(this.state.title).then(newModule => {
+      this.setState({
+        modules: this.state.modules.concat(newModule),
+        title: "",
+        id:''
+      });
+    });
+  };
+  
+  searchItem = (e) => {
+    const searchString = e.target.value
+    this.setState({searchString : searchString})
+  }  
 
   render() {
-    const { modules } = this.state;
-
-    if (modules.length > 0) {
-      return (
-        <ul>
-          {modules.map((module) => <li key={module._id}>{module.title}</li>)}
-        </ul>
-      )
-    } else {
-      return (
-        <p>There are no modules yet</p>
-      )
-    }
+    const filterModules = this.state.modules.filter((repo) => {
+      const regex = new RegExp(this.state.searchString, 'g')
+        return regex.test(repo.title)
+      })
+ 
+    return (
+      <div>
+        <div
+          className="nav"
+        >
+          <h1>
+              Digital Empowerment
+          </h1>
+          <a onClick={this.MangageUsers}>
+          </a>
+          <a onClick={this.logOut}>
+            <button>
+              Log out
+            </button>
+          </a>
+        </div>
+        <div
+          className="continer"
+        >
+            <div
+              className="Paths"
+            >
+              <h1>
+                Paths
+              </h1>
+              <input
+                className="search"
+                type='search'
+                placeholder='Search for paths...'
+                onChange={this.searchItem}
+              />
+              <input
+                className="new-path"
+                type="text"
+                placeholder="Add new module"
+                onChange={this.addNewModule}
+                value={this.state.title}
+              />
+                <button
+                  onClick={this.addModule}>
+                  Add module
+                </button>
+            </div>
+            <div class="continer_paths">
+                {filterModules.map((module) => 
+                  <div className="items_paths" key={module._id}>
+                  {module.title}
+                <button
+                  className="remove-btn"
+                  onClick={()=> {
+                    this.setState(state => ({modules: state.modules.filter(mod => module._id !== mod._id)
+                    }));
+                  }}
+              >
+              &times;
+                  </button>
+                </div>)}
+            </div>
+          </div>
+      </div>
+    );
   }
-}
-
-export default Modules;
+ }
+ export default Modules;
